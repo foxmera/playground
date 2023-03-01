9338ac3db52de973885385ba5c6e6ca59300d378
MT Fischer <marie.t.fischer@gmail.com>
2023-03-01 14:46:49 +0100

    refactor

--- python/rock-paper-scissors.py
+++ python/rock-paper-scissors.py
@@ -1,46 +1,46 @@
 # By foxmera (MT Fischer), February 2023
-# 
+#
 # ROCK PAPER SCISSORS TERMINAL GAME
 #
 
 from random import randint
 
-#create a list of play options
-t = ["Rock", "Paper", "Scissors"]
+options = ["r", "p", "s"]
+wins = ["rs", "pr", "sp"]
+win_message = {
+    "r": "Rock crushes Scissors.",
+    "p": "Paper covers Rock.",
+    "s": "Scissors cut Paper.",
+}
 
-#assign a random play to the computer
-computer = t[randint(0,2)]
 
-#set player to False
-player = False
+def play():
+    # assign a random play to the computer
+    computer = options[randint(0, 2)]
 
-while player == False:
-    #set player to True
-    player = input("Rock, Paper, Scissors? ")
-    
+    player = input("Rock, Paper, Scissors? r/p/s ")
+
+    if player not in options:
+        print("That's not a valid play. Check your spelling!")
+        play()
+        return
+
     print("Player:   " + player)
     print("Computer: " + computer)
-    
+
     if player == computer:
         print("Tie!")
-    elif player == "Rock":
-        if computer == "Paper":
-            print("You lose!", computer, "covers", player)
-        else:
-            print("You win!", player, "smashes", computer)
-    elif player == "Paper":
-        if computer == "Scissors":
-            print("You lose!", computer, "cuts", player)
-        else:
-            print("You win!", player, "covers", computer)
-    elif player == "Scissors":
-        if computer == "Rock":
-            print("You lose!", computer, "smashes", player)
-        else:
-            print("You win!", player, "cuts", computer)
+        play()
+        return
+
+    game = player + computer
+
+    if game in wins:
+        print("You win!", win_message[player])
     else:
-        print("That's not a valid play. Check your spelling!")
-        
-#player was set to True, but we want it to be False so the loop continues
-player = False
-computer = t[randint(0,2)]
+        print("You lose!", win_message[computer])
+
+    play()
+
+
+play()
